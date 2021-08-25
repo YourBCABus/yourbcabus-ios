97c945929d233f327276df8cb71bbcd837d71bd5
Anthony Li <me@anli.dev>
2021-08-25 14:03:45 -0400

    Remove notification prompt testing code

--- YourBCABus/Views/ContentView.swift
+++ YourBCABus/Views/ContentView.swift
@@ -202,7 +202,6 @@ struct ContentView: View {
                 UserDefaults.standard.set(true, forKey: "didAskToSetUpBusArrivalNotifications")
                 notificationPromptVisible = true
             }
-            notificationPromptVisible = true
             UserDefaults.standard.writeSet(starred, to: "YBBStarredBusesSet")
         }.onChange(of: dismissedAlerts) { dismissedAlerts in
             UserDefaults.standard.writeSet(dismissedAlerts, to: "YBBDismissedAlertsSet")
